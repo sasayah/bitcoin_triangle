31ee2fac32927baaf4d0045c060141fbedc9012b
sasaya hiromu <h.s.298355@gmail.com>
2017-10-24 19:06:32 +0900

    max利益の組み合わせと、利益、並び取得

---
+++ main.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+import sys
+import json
+import urllib.request
+import itertools
+
+
+def getprice(coina,coinb):
+    print('---------')
+    if coina == coinb:
+        return {'success': True, 'message': '', 'result': {'Bid': 1.0, 'Ask': 1.0, 'Last': 1.0}}
+    try:
+        url = 'https://bittrex.com/api/v1.1/public/getticker?market=' + coina + '-' + coinb
+        res = urllib.request.urlopen(url)
+        # json_loads() でPythonオブジェクトに変換
+        data = json.loads(res.read().decode('utf-8'))
+        if data['success'] == True:
+            return data
+        else:
+            new_url = 'https://bittrex.com/api/v1.1/public/getticker?market=' + coinb + '-' + coina
+            new_res = urllib.request.urlopen(new_url)
+            new_data = json.loads(new_res.read().decode('utf-8'))
+            if new_data['success'] == True:
+                last_data = new_data
+                previous_bid = new_data['result']['Bid']
+                previous_ask = new_data['result']['Ask']
+                previous_last = new_data['result']['Last']
+                last_data['result']['Bid'] = 1 / previous_ask
+                last_data['result']['Ask'] = 1 / previous_bid
+                last_data['result']['Last'] = 1 / previous_last
+                return last_data
+            else:
+                print("Invalid json response")
+                sys.exit(1)
+    except urllib.error.HTTPError as e:
+        print('HTTPError: ', e)
+    except json.JSONDecodeError as e:
+        print('JSONDecodeError: ', e)
+
+# coina -> coinb -> coinc　でそれぞれ1bitcoinの価値あたりで取引
+def triangle_trade_profit(coina, coinb, coinc):
+    coina_volume = getprice(coina,'BTC')['result']['Ask']
+    coinb_volume = getprice(coinb,'BTC')['result']['Ask']
+    coinc_volume = getprice(coinc,'BTC')['result']['Ask']
+
+    getprice_c_a = getprice(coinc,coina)['result']
+    getprice_a_b = getprice(coina,coinb)['result']
+    getprice_b_c = getprice(coinb,coinc)['result']
+
+    nexta = coinc_volume / getprice_c_a['Ask']
+    nextb = coina_volume / getprice_a_b['Ask']
+    nextc = coinb_volume / getprice_b_c['Ask']
+    profit = (nexta-coina_volume)/coina_volume + (nextb-coinb_volume)/coinb_volume + (nextc-coinc_volume)/coinc_volume
+    
+    reverse_nexta = coinb_volume * getprice_a_b['Bid']
+    reverse_nextb = coinc_volume * getprice_b_c['Bid']
+    reverse_nextc = coina_volume * getprice_c_a['Bid']
+    reverse_profit = (reverse_nexta-coina_volume)/coina_volume + (reverse_nextb-coinb_volume)/coinb_volume + (reverse_nextc-coinc_volume)/coinc_volume
+    
+    # print(profit)
+    # print(reverse_profit)
+    if profit > reverse_profit:
+        return (profit,True)
+    else:
+        return (reverse_profit,False)
+
+#print(getprice('ETH', 'USDT'))
+#print(getprice('BTC', 'USDT'))
+#print(triangle_trade_profit('BTC', 'ETH', 'USDT'))
+#print(triangle_trade_profit('ETH', 'BTC', 'USDT'))
+
+def search_max_profit(coinlist):
+    max_profit_list = ['','','']
+    max_profit = 0.0
+    true_reverse = True
+    coin_pairs = list(itertools.combinations(coinlist, 3))
+    for coin_pair in coin_pairs:
+        profit, true_reverse = triangle_trade_profit(coin_pair[0],coin_pair[1],coin_pair[2])
+        if  max_profit < profit:
+            max_profit = profit
+            max_profit_list = coin_pair
+    return (max_profit_list, true_reverse, max_profit)
+
+        
+
+
+def main():
+    coinlist = ['ETH', 'BTC', 'USDT', 'LTC']
+    max_profit_list, true_reverse, max_profit = search_max_profit(coinlist)
+    print(max_profit_list)
+    print(max_profit)
+    print(true_reverse)
+    # print(list(itertools.combinations(coinlist, 3)))
+
+
+main()
