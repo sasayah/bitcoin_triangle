7d41b62464e199ac53a9489c957a4dad65ad43dc
sasaya hiromu <h.s.298355@gmail.com>
2017-10-25 22:31:22 +0900

    get_json_data,get_json_key_dataでコードを整理

--- main.py
+++ main.py
@@ -5,41 +5,34 @@ import urllib.request
 import itertools
 import os
 from time import sleep
+import hmac
+import hashlib
+
 
 
 def getprice(coina,coinb):
     print('---------')
     if coina == coinb:
         return {'success': True, 'message': '', 'result': {'Bid': 1.0, 'Ask': 1.0, 'Last': 1.0}}
-    try:
-        url = 'https://bittrex.com/api/v1.1/public/getticker?market=' + coina + '-' + coinb
-        res = urllib.request.urlopen(url)
-        # json_loads() でPythonオブジェクトに変換
-        data = json.loads(res.read().decode('utf-8'))
-        if data['success'] == True:
-            return data
+    url = 'https://bittrex.com/api/v1.1/public/getticker?market=' + coina + '-' + coinb
+    data = get_json_data(url)
+    if data['success']:
+        return data
+    else:
+        new_url = 'https://bittrex.com/api/v1.1/public/getticker?market=' + coinb + '-' + coina
+        new_data = get_json_data(new_url)
+        if new_data['success']:
+            last_data = new_data
+            previous_bid = new_data['result']['Bid']
+            previous_ask = new_data['result']['Ask']
+            previous_last = new_data['result']['Last']
+            last_data['result']['Bid'] = 1 / previous_ask
+            last_data['result']['Ask'] = 1 / previous_bid
+            last_data['result']['Last'] = 1 / previous_last
+            return last_data
         else:
-            new_url = 'https://bittrex.com/api/v1.1/public/getticker?market=' + coinb + '-' + coina
-            new_res = urllib.request.urlopen(new_url)
-            new_data = json.loads(new_res.read().decode('utf-8'))
-            if new_data['success'] == True:
-                last_data = new_data
-                previous_bid = new_data['result']['Bid']
-                previous_ask = new_data['result']['Ask']
-                previous_last = new_data['result']['Last']
-                last_data['result']['Bid'] = 1 / previous_ask
-                last_data['result']['Ask'] = 1 / previous_bid
-                last_data['result']['Last'] = 1 / previous_last
-                return last_data
-            else:
-                print("Invalid json response")
-                sys.exit(1)
-    except urllib.error.HTTPError as e:
-        print('HTTPError: ', e)
-        sys.exit(1)
-    except json.JSONDecodeError as e:
-        print('JSONDecodeError: ', e)
-        sys.exit(1)
+            print("Invalid json response")
+            sys.exit(1)
 
 # coina -> coinb -> coinc　でそれぞれ1bitcoinの価値あたりで取引
 def triangle_trade_profit(coina, coinb, coinc):
@@ -73,6 +66,7 @@ def triangle_trade_profit(coina, coinb, coinc):
 #print(triangle_trade_profit('BTC', 'ETH', 'USDT'))
 #print(triangle_trade_profit('ETH', 'BTC', 'USDT'))
 
+
 def search_max_profit(coinlist):
     max_profit_list = ['','','']
     max_profit = 0.0
@@ -87,59 +81,35 @@ def search_max_profit(coinlist):
 
         #エラー処理!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!apiを叩くとこは全てエラー処理
 def buy_coin(coina, coinb, quantity, rate):
-    try:
-        url = 'https://bittrex.com/api/v1.1/market/buylimit?apikey=' + os.environ["API_KEY"] + '&market=' + coina + '-' + coinb + '&quantity=' + str(quantity) +'&rate=' + str(rate)
-        res = urllib.request.urlopen(url)
-        #data['result']['uuid']に取引IDを記録
-        data = json.loads(res.read().decode('utf-8'))
-        return data
-    except urllib.error.HTTPError as e:
-        print('HTTPError: ', e)
-        sys.exit(1)
-    except json.JSONDecodeError as e:
-        print('JSONDecodeError: ', e)
-        sys.exit(1)
+    url = 'https://bittrex.com/api/v1.1/market/buylimit?apikey=' 
+    url += os.environ["API_KEY"] 
+    url += '&market=' + coina + '-' + coinb 
+    url += '&quantity=' + str(quantity) 
+    url += '&rate=' + str(rate)
+    data = get_json_key_data(url)
+    return data
 
 #　現在のコイン保有量を取得
 def account_money_amount(coina,coinb,coinc):
-    try:
-        url_a = 'https://bittrex.com/api/v1.1/account/getbalance?apikey=' + os.environ["API_KEY"] + '&currency=' + coina
-        url_b = 'https://bittrex.com/api/v1.1/account/getbalance?apikey=' + os.environ["API_KEY"] + '&currency=' + coinb
-        url_c = 'https://bittrex.com/api/v1.1/account/getbalance?apikey=' + os.environ["API_KEY"] + '&currency=' + coinc
-
-        res_a = urllib.request.urlopen(url_a)
-        res_b = urllib.request.urlopen(url_b)
-        res_c = urllib.request.urlopen(url_c)
-
-        data_a = json.loads(res_a.read().decode('utf-8'))
-        data_b = json.loads(res_b.read().decode('utf-8'))
-        data_c = json.loads(res_c.read().decode('utf-8'))
-        #現在所有のビットコインを確定!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        print(url_a)
-        print(data_a)
-        curretn_coin_a_ammout = data_a['result']['Available']
-        curretn_coin_b_ammout = data_b['result']['Available']
-        curretn_coin_c_ammout = data_c['result']['Available']
-        return (curretn_coin_a_ammout, curretn_coin_b_ammout, curretn_coin_c_ammout)
-    except urllib.error.HTTPError as e:
-        print('HTTPError: ', e)
-        sys.exit(1)
-    except json.JSONDecodeError as e:
-        print('JSONDecodeError: ', e)
-        sys.exit(1)
+    url_a = 'https://bittrex.com/api/v1.1/account/getbalance?apikey=' + os.environ["API_KEY"] + '&currency=' + coina
+    url_b = 'https://bittrex.com/api/v1.1/account/getbalance?apikey=' + os.environ["API_KEY"] + '&currency=' + coinb
+    url_c = 'https://bittrex.com/api/v1.1/account/getbalance?apikey=' + os.environ["API_KEY"] + '&currency=' + coinc
+    
+    data_a = get_json_key_data(url_a)
+    data_b = get_json_key_data(url_b)
+    data_c = get_json_key_data(url_c)
+    #現在所有のビットコインを確定!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    print(url_a)
+    print(data_a)
+    curretn_coin_a_ammout = data_a['result']['Available']
+    curretn_coin_b_ammout = data_b['result']['Available']
+    curretn_coin_c_ammout = data_c['result']['Available']
+    return (curretn_coin_a_ammout, curretn_coin_b_ammout, curretn_coin_c_ammout)
 
 def cancel_trade(uuid):
-    try:
-        url = 'https://bittrex.com/api/v1.1/market/cancel?apikey=' + os.environ["API_KEY"] + '&uuid=' + uuid
-        res = urllib.request.urlopen(url)
-        data = json.loads(res.read().decode('utf-8'))
-        return data
-    except urllib.error.HTTPError as e:
-        print('HTTPError: ', e)
-        sys.exit(1)
-    except json.JSONDecodeError as e:
-        print('JSONDecodeError: ', e)
-        sys.exit(1)
+    url = 'https://bittrex.com/api/v1.1/market/cancel?apikey=' + os.environ["API_KEY"] + '&uuid=' + uuid
+    data = get_json_key_data(url)
+    return data
 
 
 def execute_triangle(coinlist,minprofit):
@@ -153,8 +123,8 @@ def execute_triangle(coinlist,minprofit):
         getprice_c_a = getprice(coinc,coina)['result']
         getprice_a_b = getprice(coina,coinb)['result']
         getprice_b_c = getprice(coinb,coinc)['result']
-        #追加！！！！！!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         curretn_coin_a_ammout, curretn_coin_b_ammout, curretn_coin_c_ammout = account_money_amount(coina,coinb,coinc)
+        #ここは三種類の価値を統一にすべき
         sell_coin_a_amount = curretn_coin_a_ammout * 0.8
         sell_coin_b_amount = curretn_coin_b_ammout * 0.8
         sell_coin_c_amount = curretn_coin_c_ammout * 0.8
@@ -172,6 +142,34 @@ def execute_triangle(coinlist,minprofit):
         cancel_trade(trade_data_a['result']['uuid'])
         cancel_trade(trade_data_b['result']['uuid'])
         cancel_trade(trade_data_c['result']['uuid'])
+
+
+def get_json_data(url):
+    try:
+        res = urllib.request.urlopen(url)
+        data = json.loads(res.read().decode('utf-8'))
+        return data
+    except urllib.error.HTTPError as e:
+        print('HTTPError: ', e)
+        sys.exit(1)
+    except json.JSONDecodeError as e:
+        print('JSONDecodeError: ', e)
+        sys.exit(1)
+
+
+def get_json_key_data(url):
+    try:
+        signing = hmac.new(os.environ["API_SECRET"].encode("UTF-8"), url.encode("UTF-8"), hashlib.sha512).hexdigest()
+        headers = {'apisign': signing}
+        res = urllib.request.Request(url, headers=headers)
+        data = json.loads(urllib.request.urlopen(res).read())
+        return data
+    except urllib.error.HTTPError as e:
+        print('HTTPError: ', e)
+        sys.exit(1)
+    except json.JSONDecodeError as e:
+        print('JSONDecodeError: ', e)
+        sys.exit(1)
 
 
 def main():
